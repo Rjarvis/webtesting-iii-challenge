5f3429ed77cdb0648abc843be5e345755e2a234b
Roland J Jarvis <Rjarvis87@gmail.com>
2019-10-05 14:47:39 -0400

    commit before stuff

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react';
+import { render, fireEvent, act } from 'react-testing-library'
+
+import Controls from './Controls';
+
+it('renders Controls locked', () => {
+    const { getByText, findByText } = render(<Controls />);
+    getByText("notAThing");
+    findByText("notAThing");
+});
